d8a04b901eae125777c207581a18c1ff264389f1
Kwon770 <sckwon770@gmail.com>
2022-05-31 22:56:37 +0900

    chore: project environment init

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
+<
+src/index.js,f/9/f900edc44cb929c2267a16100a32851f6d5b477d
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Home = () => {
+  return <Background>asd</Background>;
+};
+
+const Background = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: gray;
+`;
+
+export default Home;
