a8812bfd7de6d4f9d797e14f9af6624bf4d0d186
j8won <joowoni99@gmail.com>
2022-07-08 02:56:33 +0900

    feat: Add Banner, Banners

---
+++ src/Components/Banner.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Banner({
+  href, bgColor, text, src,
+}) {
+  return (
+    <Link href={href}>
+      <Box bgColor={bgColor}>
+        <Img src={src} />
+        <Text text={text}> {text} </Text>
+      </Box>
+    </Link>
+  );
+}
+
+const Box = styled.div`
+  width: 100%;
+  height: 12.5vh;
+  
+  background-color: ${(props) => props.bgColor};
+  border-radius: 20px;
+  
+  display: flex;
+r;  align-items: center;
+`;
+
+const Link = styled.a`
+  text-decoration-line: none;
+  
+  :visited {
+    color: ${(props) => props.theme.bgColor};
+  }
+`;
+
+const Img = styled.img``;
+
+const Text = styled.h1`
+  color: ${(props) => props.theme.bgColor};
+  font-size: 1rem;
+  
+  margin-left: 1rem;
+`;
+
+export default Banner;
