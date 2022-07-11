8234c6bc29bd175f4241b7bbd621113549c9ea53
j8won <joowoni99@gmail.com>
2022-07-11 16:22:40 +0900

    feat: add css in Classifications

--- src/Components/Classifications.js
+++ src/Components/Classifications.js
@@ -3,32 +3,64 @@ import styled from 'styled-components';
 import Class from './Class';
 
 function Classifications() {
+  const firstRowItems = [
+    { src: 'https://images.vexels.com/media/users/3/205387/isolated/preview/9e5a4a16e78a187fc3e47fc6e2c5f03a-internet-website-icon-stroke.png', to: '/web', text: 'WEB' },
+    { src: 'https://seeklogo.com/images/A/android-icon-logo-DB06FA8B39-seeklogo.com.png', to: '/app', text: 'APP' },
+    { src: 'https://images.vexels.com/media/users/3/205387/isolated/preview/9e5a4a16e78a187fc3e47fc6e2c5f03a-internet-website-icon-stroke.png', to: '/web', text: 'WEB' },
+    { src: 'https://seeklogo.com/images/A/android-icon-logo-DB06FA8B39-seeklogo.com.png', to: '/app', text: 'APP' },
+  ];
+
+  const secondRowItems = [
+    { src: 'https://images.vexels.com/media/users/3/205387/isolated/preview/9e5a4a16e78a187fc3e47fc6e2c5f03a-internet-website-icon-stroke.png', to: '/web', text: 'WEB' },
+    { src: 'https://seeklogo.com/images/A/android-icon-logo-DB06FA8B39-seeklogo.com.png', to: '/app', text: 'APP' },
+    { src: 'https://images.vexels.com/media/users/3/205387/isolated/preview/9e5a4a16e78a187fc3e47fc6e2c5f03a-internet-website-icon-stroke.png', to: '/web', text: 'WEB' },
+    { src: 'https://seeklogo.com/images/A/android-icon-logo-DB06FA8B39-seeklogo.com.png', to: '/app', text: 'APP' },
+  ];
   return (
-    <Box>
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-      <Class src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png" to="/react" text="React" />
-    </Box>
+    <ClassBox>
+      <Box>
+        {firstRowItems.map((item) => (
+          <Class src={item.src} to={item.to} text={item.text} />
+        ))}
+      </Box>
+      <Box>
+        {secondRowItems.map((item) => (
+          <Class src={item.src} to={item.to} text={item.text} />
+        ))}
+      </Box>
+    </ClassBox>
   );
 }
 
 const Box = styled.div`
   display: flex;
+  flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  
+  width: 100vw;
+  
+  @media screen and (min-width: 767px) {
+    width: 60vw;
+  }
+`;
+
+const ClassBox = styled.div`
+  display: flex;
+  flex-direction: row;
   flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 90vw;
   
   position: absolute;
   top: 60vh;
   left: 5vw;
   
-
- width: 90vw;
+  @media screen and (min-width: 767px) {
+    left: 20vw;
+  }
 `;
 
 export default Classifications;
