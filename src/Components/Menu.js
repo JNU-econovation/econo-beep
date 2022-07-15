b043fdf63e68a2d2f8d2fd411bf9515719c1fb4a
j8won <joowoni99@gmail.com>
2022-07-16 02:12:54 +0900

    feat: Add animation in MenuButtonHolder

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,35 +1,50 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function Menu({ isToggled }) {
   return (
-
-    <MenuButtonHolder isToggled={isToggled}>
+    <MenuButtonsHolder isToggled={isToggled}>
       <StyledButton component={Link} to="/books" variant="text" color="info">도서</StyledButton>
       <StyledButton component={Link} to="/equipment" variant="text" color="info">기자재</StyledButton>
       <StyledButton component={Link} to="/manager" variant="text" color="info">관리자</StyledButton>
-    </MenuButtonHolder>
-
+    </MenuButtonsHolder>
   );
 }
 
-const MenuButtonHolder = styled.ul`
+const menuAnimation = keyframes`
+  from {
+    transform: translateY(-30vh);
+  }
+  to {
+    transfrom: translateY(0vh)
+  }
+`;
+
+const MenuButtonsHolder = styled.ul`
   list-style: none;
   display: flex;
   width: 50vw;
+  
+  background-color: ${(props) => props.theme.bgColor};
   
   @media screen and (max-width: 767px) {
     display: ${(props) => (props.isToggled ? 'flex' : 'none')};
     flex-direction: column;
     align-items: center;
+    
     width: 100%;
     margin: 0;
     padding: 0 1rem;
 
     border-top: ${(props) => props.theme.borderTop};    
     box-shadow: ${(props) => props.theme.boxShadow};
+    
+    ${(props) => props.isToggled
+      && css`
+      animation: ${menuAnimation} 500ms ease-in-out;
+    `}
   }
   
 `;
@@ -37,11 +52,10 @@ const MenuButtonHolder = styled.ul`
 const StyledButton = styled(Button)`
   && {
     padding: 0 5vw;
-    
     font-size: 1rem;
 
     @media screen and (max-width: 767px) {
-      margin: 1rem 1rem;
+      margin: 1rem;
       padding: 0;
     }
     
