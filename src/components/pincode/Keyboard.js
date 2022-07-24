f88608de36120d65c1de48ebf7d60a8bdc1f9bba
j8won <joowoni99@gmail.com>
2022-07-25 03:38:15 +0900

    feat: Add border-top in KeyboardBox

--- src/components/pincode/Keyboard.js
+++ src/components/pincode/Keyboard.js
@@ -3,32 +3,32 @@ import styled from 'styled-components';
 
 function Keyboard({ addNumber, deleteNumber, sendNumber }) {
   return (
-    <KeyBoardBox>
-      <KeyBoardColumn>
-        <KeyBoardButton value='1' onClick={addNumber}>1</KeyBoardButton>
-        <KeyBoardButton value='2' onClick={addNumber}>2</KeyBoardButton>
-        <KeyBoardButton value='3' onClick={addNumber}>3</KeyBoardButton>
-      </KeyBoardColumn>
-      <KeyBoardColumn>
-        <KeyBoardButton value='4' onClick={addNumber}>4</KeyBoardButton>
-        <KeyBoardButton value='5' onClick={addNumber}>5</KeyBoardButton>
-        <KeyBoardButton value='6' onClick={addNumber}>6</KeyBoardButton>
-      </KeyBoardColumn>
-      <KeyBoardColumn>
-        <KeyBoardButton value='7' onClick={addNumber}>7</KeyBoardButton>
-        <KeyBoardButton value='8' onClick={addNumber}>8</KeyBoardButton>
-        <KeyBoardButton value='9' onClick={addNumber}>9</KeyBoardButton>
-      </KeyBoardColumn>
-      <KeyBoardColumn>
-        <KeyBoardTextButton onClick={deleteNumber}>삭제</KeyBoardTextButton>
-        <KeyBoardButton value='0' onClick={addNumber}>0</KeyBoardButton>
-        <KeyBoardTextButton onClick={sendNumber}>확인</KeyBoardTextButton>
-      </KeyBoardColumn>
-    </KeyBoardBox>
+    <KeyboardBox>
+      <KeyboardColumn>
+        <KeyboardButton value='1' onClick={addNumber}>1</KeyboardButton>
+        <KeyboardButton value='2' onClick={addNumber}>2</KeyboardButton>
+        <KeyboardButton value='3' onClick={addNumber}>3</KeyboardButton>
+      </KeyboardColumn>
+      <KeyboardColumn>
+        <KeyboardButton value='4' onClick={addNumber}>4</KeyboardButton>
+        <KeyboardButton value='5' onClick={addNumber}>5</KeyboardButton>
+        <KeyboardButton value='6' onClick={addNumber}>6</KeyboardButton>
+      </KeyboardColumn>
+      <KeyboardColumn>
+        <KeyboardButton value='7' onClick={addNumber}>7</KeyboardButton>
+        <KeyboardButton value='8' onClick={addNumber}>8</KeyboardButton>
+        <KeyboardButton value='9' onClick={addNumber}>9</KeyboardButton>
+      </KeyboardColumn>
+      <KeyboardColumn>
+        <KeyboardTextButton onClick={deleteNumber}>삭제</KeyboardTextButton>
+        <KeyboardButton value='0' onClick={addNumber}>0</KeyboardButton>
+        <KeyboardTextButton onClick={sendNumber}>확인</KeyboardTextButton>
+      </KeyboardColumn>
+    </KeyboardBox>
   );
 }
 
-const KeyBoardBox = styled.div`
+const KeyboardBox = styled.div`
   width: 100%;
   height: 50%;
   
@@ -36,14 +36,15 @@ const KeyBoardBox = styled.div`
   bottom: 0;
   
   background-color: ${(props) => props.theme.bgColor};
+  border-top: ${(props) => props.theme.borderTop};
 `;
 
-const KeyBoardColumn = styled.div`
+const KeyboardColumn = styled.div`
   width: 100%;
   height: 25%;
 `;
 
-const KeyBoardButton = styled.button`
+const KeyboardButton = styled.button`
   width: 33.33%;
   height: 100%;
   
@@ -58,7 +59,7 @@ const KeyBoardButton = styled.button`
   font-size: 1.5rem;
 `;
 
-const KeyBoardTextButton = styled(KeyBoardButton)`
+const KeyboardTextButton = styled(KeyboardButton)`
   font-size: 1rem;
 `;
 
