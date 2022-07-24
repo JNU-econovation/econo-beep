94117c0da1eb83ba265da6cffb3ea41dc75169ed
Kwon770 <sckwon770@gmail.com>
2022-07-25 08:57:11 +0900

    refactor: feat#30 code review

--- src/components/List/ListResultBox.js
+++ src/components/List/ListResultBox.js
@@ -7,13 +7,6 @@ const ListResultBox = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-
-  @media screen and (min-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  }
 `;
 
 export default ListResultBox;
--- src/components/List/ListSearchBarHolder.js
+++ src/components/List/ListSearchBarHolder.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const ListSearchBarHolder = styled.div`
   width: 90%;
   max-width: 1000px;
-  margin: 1rem 0;
+  margin: 1.8vh 0;
 `;
 
 export default ListSearchBarHolder;
