b1ef4292d968e930ba94a9a41838f78ccd843849
j8won <joowoni99@gmail.com>
2022-07-29 05:44:38 +0900

    feat: restore components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,17 +12,19 @@ function Footer() {
 const FooterBox = styled.footer`
   width: 100vw;
   height: 10vh;
-  
+
   position: absolute;
   bottom: 0;
-  
+
   margin-top: 10px;
   padding: 1.5rem;
-  
+
   color: ${(props) => props.theme.firstGray};
   border-top: 1px solid ${(props) => props.theme.borderGray};
-  
+
   font-size: 0.8rem;
 `;
 
 export default Footer;
+
+
